e95326996a47c683e9f3fd4b2118186ccd953806
彭超 <3402977740@qq.com>
2023-11-29 23:00:20 +0800

    增加向量数据库模块

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from nodoc.structure import docNode
 from nodoc.structure import docTree
 from nodoc.structure import Node
 from nodoc.structure import Tree
+from nodoc.database import vectorDB
 
 __all__ = [
     'docNode',
